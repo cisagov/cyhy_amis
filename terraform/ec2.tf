1d05ec88bd3dda17d036afd9ebd9e24d74f0fdc4
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-07-29 17:14:22 -0400

    Mongo instance now running in a VPC

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,3 +1,4 @@
+# The Mongo AMI that we are starting
 data "aws_ami" "mongo" {
   filter {
     name = "name"
@@ -20,6 +21,7 @@ data "aws_ami" "mongo" {
   most_recent = true
 }
 
+# The EC2 instance that is running MongoDB
 resource "aws_instance" "mongo" {
   ami = "${data.aws_ami.mongo.id}"
   instance_type = "t2.micro"
@@ -32,13 +34,16 @@ resource "aws_instance" "mongo" {
     delete_on_termination = true
   }
 
-  security_groups = [
-    "${aws_security_group.mongo_sg.name}"
+  associate_public_ip_address = true
+  subnet_id = "${aws_subnet.mongo_public_subnet.id}"
+  vpc_security_group_ids = [
+    "${aws_security_group.mongo_public_sg.id}"
   ]
 
   tags = "${var.tags}"
 }
 
+# Mongo data volume
 resource "aws_ebs_volume" "mongo_data" {
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
   type = "io1"
@@ -48,6 +53,7 @@ resource "aws_ebs_volume" "mongo_data" {
   tags = "${var.tags}"
 }
 
+# Mongo journal volume
 resource "aws_ebs_volume" "mongo_journal" {
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
   type = "io1"
@@ -57,6 +63,7 @@ resource "aws_ebs_volume" "mongo_journal" {
   tags = "${var.tags}"
 }
 
+# Mongo log volume
 resource "aws_ebs_volume" "mongo_log" {
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
   type = "io1"
@@ -66,38 +73,23 @@ resource "aws_ebs_volume" "mongo_log" {
   tags = "${var.tags}"
 }
 
+# Attach the Mongo data volume to the EC2 instance
 resource "aws_volume_attachment" "mongo_data_attachment" {
   device_name = "/dev/xvdb"
   volume_id = "${aws_ebs_volume.mongo_data.id}"
   instance_id = "${aws_instance.mongo.id}"
 }
 
+# Attach the Mongo journal volume to the EC2 instance
 resource "aws_volume_attachment" "mongo_journal_attachment" {
   device_name = "/dev/xvdc"
   volume_id = "${aws_ebs_volume.mongo_journal.id}"
   instance_id = "${aws_instance.mongo.id}"
 }
 
+# Attach the Mongo log volume to the EC2 instance
 resource "aws_volume_attachment" "mongo_log_attachment" {
   device_name = "/dev/xvdd"
   volume_id = "${aws_ebs_volume.mongo_log.id}"
   instance_id = "${aws_instance.mongo.id}"
-}
-
-resource "aws_security_group" "mongo_sg" {
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = "${var.tags}"
 }
