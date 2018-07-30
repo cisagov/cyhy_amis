f4d533b399e1b072b96436e5bb1d1cbd83ec7f5f
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-07-30 13:14:19 -0400

    Put teh docker host in the private subnet, not the public

--- terraform_bod/docker_ec2.tf
+++ terraform_bod/docker_ec2.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "docker" {
   # ebs_optimized = true
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
 
-  # This is the public subnet
+  # This is the private subnet
   subnet_id = "${aws_subnet.bod_private_subnet.id}"
 
   root_block_device {
@@ -38,7 +38,7 @@ resource "aws_instance" "docker" {
   }
 
   vpc_security_group_ids = [
-    "${aws_security_group.bod_public_sg.id}"
+    "${aws_security_group.bod_private_sg.id}"
   ]
 
   tags = "${merge(var.tags, map("Name", "BOD 18-01 Docker host"))}"
