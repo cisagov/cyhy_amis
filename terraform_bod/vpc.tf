7270ffef11483c8377f935e8039de8b3cb210075
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-08-01 18:11:29 -0400

    Fix a bug where the Docker host could not egress via teh NAT gateway.
    Add egress via port 53 (TCP and UDP) so the Docker host can perform
    lookups using Google's DNS.

--- terraform_bod/vpc.tf
+++ terraform_bod/vpc.tf
@@ -183,12 +183,35 @@ resource "aws_network_acl_rule" "private_egress_anywhere_via_port_587" {
   to_port = 587
 }
 
+# Allow egress anywhere via DNS.  This is so the NAT gateway can relay
+# the corresponding requests from the private subnet.
+resource "aws_network_acl_rule" "private_egress_anywhere_via_dns_tcp" {
+  network_acl_id = "${aws_network_acl.bod_private_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 150
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 53
+  to_port = 53
+}
+resource "aws_network_acl_rule" "private_egress_anywhere_via_dns_udp" {
+  network_acl_id = "${aws_network_acl.bod_private_acl.id}"
+  egress = true
+  protocol = "udp"
+  rule_number = 151
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 53
+  to_port = 53
+}
+
 # Allow egress to the public subnet via ephemeral ports
 resource "aws_network_acl_rule" "private_egress_to_public_via_ephemeral_ports" {
   network_acl_id = "${aws_network_acl.bod_private_acl.id}"
   egress = true
   protocol = "tcp"
-  rule_number = 150
+  rule_number = 160
   rule_action = "allow"
   cidr_block = "${aws_subnet.bod_public_subnet.cidr_block}"
   from_port = 1024
@@ -205,6 +228,35 @@ resource "aws_network_acl" "bod_public_acl" {
   tags = "${merge(var.tags, map("Name", "BOD 18-01 Public"))}"
 }
 
+# Allow ingress from the private subnet via any port and protocol.
+# This allows EC2 instances in the private subnet to send any traffic
+# they want via the NAT gateway, subject to their own security group
+# and network ACL restrictions.
+resource "aws_network_acl_rule" "public_ingress_from_private_via_all_ports_and_protocols" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = false
+  protocol = "-1"
+  rule_number = 80
+  rule_action = "allow"
+  cidr_block = "${aws_subnet.bod_private_subnet.cidr_block}"
+  from_port = 0
+  to_port = 0
+}
+
+# Allow ingress from anywhere via ephemeral ports.  This is necessary
+# because the return traffic to the NAT gateway has to enter here
+# before it is relayed to the private subnet.
+resource "aws_network_acl_rule" "public_ingress_from_anywhere_via_ephemeral_ports" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = false
+  protocol = "tcp"
+  rule_number = 90
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 1024
+  to_port = 65535
+}
+
 # Allow ingress from anywhere via ssh
 resource "aws_network_acl_rule" "public_ingress_from_anywhere_via_ssh" {
   network_acl_id = "${aws_network_acl.bod_public_acl.id}"
@@ -215,18 +267,6 @@ resource "aws_network_acl_rule" "public_ingress_from_anywhere_via_ssh" {
   cidr_block = "0.0.0.0/0"
   from_port = 22
   to_port = 22
-}
-
-# Allow ingress from the private subnet via ephemeral ports
-resource "aws_network_acl_rule" "public_ingress_from_private_via_ephemeral_ports" {
-  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
-  egress = false
-  protocol = "tcp"
-  rule_number = 110
-  rule_action = "allow"
-  cidr_block = "${aws_subnet.bod_private_subnet.cidr_block}"
-  from_port = 1024
-  to_port = 65535
 }
 
 # Allow egress to the private subnet via ssh
@@ -241,12 +281,92 @@ resource "aws_network_acl_rule" "public_egress_to_private_via_ssh" {
   to_port = 22
 }
 
+# Allow egress anywhere via http and https.  This is so the NAT
+# gateway can relay the corresponding requests from the private
+# subnet.
+resource "aws_network_acl_rule" "public_egress_anywhere_via_http" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 140
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 80
+  to_port = 80
+}
+resource "aws_network_acl_rule" "public_egress_anywhere_via_https" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 141
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
+}
+
+# Allow egress anywhere via SMTP.  This is so the NAT gateway can
+# relay the corresponding requests from the private subnet.
+resource "aws_network_acl_rule" "public_egress_anywhere_via_port_25" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 150
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 25
+  to_port = 25
+}
+resource "aws_network_acl_rule" "public_egress_anywhere_via_port_465" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 151
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 465
+  to_port = 465
+}
+resource "aws_network_acl_rule" "public_egress_anywhere_via_port_587" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 152
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 587
+  to_port = 587
+}
+
+# Allow egress anywhere via DNS.  This is so the NAT gateway can relay
+# the corresponding requests from the private subnet.
+resource "aws_network_acl_rule" "public_egress_anywhere_via_dns_tcp" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 160
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 53
+  to_port = 53
+}
+resource "aws_network_acl_rule" "public_egress_anywhere_via_dns_udp" {
+  network_acl_id = "${aws_network_acl.bod_public_acl.id}"
+  egress = true
+  protocol = "udp"
+  rule_number = 161
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+  from_port = 53
+  to_port = 53
+}
+
 # Allow egress to anywhere via ephemeral ports
 resource "aws_network_acl_rule" "public_egress_to_anywhere_via_ephemeral_ports" {
   network_acl_id = "${aws_network_acl.bod_public_acl.id}"
   egress = true
   protocol = "tcp"
-  rule_number = 140
+  rule_number = 170
   rule_action = "allow"
   cidr_block = "0.0.0.0/0"
   from_port = 1024
@@ -268,18 +388,6 @@ resource "aws_security_group_rule" "docker_ssh_ingress_from_bastion" {
   source_security_group_id = "${aws_security_group.bod_bastion_sg.id}"
   from_port = 22
   to_port = 22
-}
-
-# Allow ephemeral ports from anywhere
-resource "aws_security_group_rule" "docker_ephemeral_ports_from_anywhere" {
-  security_group_id = "${aws_security_group.bod_docker_sg.id}"
-  type = "ingress"
-  protocol = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-  from_port = 1024
-  to_port = 65535
 }
 
 # Allow HTTP and HTTPS egress anywhere
