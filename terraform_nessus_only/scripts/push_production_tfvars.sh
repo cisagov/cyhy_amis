b3b87e1b7ef8a1d8e55d126f09d2a9140fe4f01d
Dave Redmin <david.redmin@beta.dhs.gov>
2018-08-01 16:25:42 -0400

    Add new script to push local version of production.tfvars to the correct S3 bucket, so it can be used by others

---
+++ terraform_nessus_only/scripts/push_production_tfvars.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Push your current local version of production.tfvars to the correct
+# S3 bucket so that it can be used by others
+# Requirement: AWS command line interface must be installed/setup on your system
+
+TERRAFORM_TFVARS_S3_BUCKET="ncats-terraform-production-tfvars"
+TERRAFORM_DIR="terraform_nessus_only"
+TFVARS_FILE="production.tfvars"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+aws s3 cp ${SCRIPT_DIR}/../${TFVARS_FILE} s3://${TERRAFORM_TFVARS_S3_BUCKET}/${TERRAFORM_DIR}/${TFVARS_FILE}
