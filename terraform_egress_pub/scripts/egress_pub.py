5560d6f14ecb99d8535b0880f9ee14297d604e26
Mark Feldhousen <markf@brego.local>
2018-08-08 22:23:33 -0700

    fix bug with instances that have no public ip sort ip list in output change protocol in file comment to https

--- terraform_egress_pub/scripts/egress_pub.py
+++ terraform_egress_pub/scripts/egress_pub.py
@@ -3,17 +3,17 @@ import boto3
 from datetime import datetime
 
 BUCKET_NAME = 's3-cdn.rules.ncats.cyber.dhs.gov'
-DOMAIN = BUCKET_NAME
+DOMAIN = 'rules.ncats.cyber.dhs.gov'
+STATIC_IPS = ('64.69.57.0/24',)
 FILE_NAME = 'all.txt'
 HEADER = '''###
-# http://{domain}/{filename}
+# https://{domain}/{filename}
 # {timestamp}
 # DHS National Cybersecurity Assessments & Technical Services (NCATS)
 # This file contains a consolidated list of all the IP addresses that NCATS is
 # currently using for external scanning.
 # Please contact ncats@hq.dhs.gov with any questions
 ###
-64.69.57.0/24
 '''
 
 def main():
@@ -23,12 +23,21 @@ def main():
     ec2 = boto3.resource('ec2')
     instances = ec2.instances.filter(
         Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+    # start with the static set of IPs and add additional public ips
+    ips = set(STATIC_IPS)
     for instance in instances:
         print(instance.id, instance.instance_type, instance.public_ip_address)
-        if instance.public_ip_address != None: 
-            HEADER += instance.public_ip_address + '\n'
+        ips.add(instance.public_ip_address)
 
-    # Fill in template
+    # if a None was put in the set, remove it
+    ips.remove(None)
+
+    # append sorted ips to the header
+    for ip in sorted(ips):
+        HEADER += ip + '\n'
+
+    # fill in template
     now = '{0:%a %b %d %H:%M:%S UTC %Y}'.format(datetime.utcnow())
     HEADER = HEADER.format(domain=DOMAIN, filename=FILE_NAME, timestamp=now)
 
