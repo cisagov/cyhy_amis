e4e5cbd6f48b001799bc26e8dd77eed1848183c9
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-08-15 17:44:54 -0400

    Tell cloud init how to merge the two lists of users

--- terraform/cyhy_ssh_cloud_init.tf
+++ terraform/cyhy_ssh_cloud_init.tf
@@ -14,10 +14,11 @@ data "template_cloudinit_config" "cyhy_ssh_cloud_init_tasks" {
     content      = "${data.template_file.user_ssh_setup.rendered}"
   }
 
-#  part {
-#    filename     = "cyhy_user_ssh_setup.yml"
-#    content_type = "text/cloud-config"
-#    content      = "${data.template_file.cyhy_user_ssh_setup.rendered}"
-#  }
+  part {
+    filename     = "cyhy_user_ssh_setup.yml"
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.cyhy_user_ssh_setup.rendered}"
+    merge_type   = "list(append)+dict(recurse_array)+str()"
+  }
 }
 
