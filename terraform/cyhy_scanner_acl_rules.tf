05a28c54a526960b961a75279af97f0adf6059b6
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-08-15 17:04:43 -0400

    Remove some comments that are no longer relevant

--- terraform/cyhy_scanner_acl_rules.tf
+++ terraform/cyhy_scanner_acl_rules.tf
@@ -1,8 +1,4 @@
 # Allow ingress from anywhere via the Nessus UI port
-#
-# TODO - This should be locked down using
-# var/trusted_ingress_networks_ipv4 and
-# var/trusted_ingress_networks_ipv6
 resource "aws_network_acl_rule" "scanner_ingress_from_anywhere_via_nessus" {
   network_acl_id = "${aws_network_acl.cyhy_scanner_acl.id}"
   egress = false
@@ -15,10 +11,6 @@ resource "aws_network_acl_rule" "scanner_ingress_from_anywhere_via_nessus" {
 }
 
 # Allow ingress from anywhere via ssh
-#
-# TODO - This should be locked down using
-# var/trusted_ingress_networks_ipv4 and
-# var/trusted_ingress_networks_ipv6
 resource "aws_network_acl_rule" "scanner_ingress_from_anywhere_via_ssh" {
   network_acl_id = "${aws_network_acl.cyhy_scanner_acl.id}"
   egress = false
