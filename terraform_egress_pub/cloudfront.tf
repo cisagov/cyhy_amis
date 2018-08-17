d8ad1fe8715454319c5d18ee10d8199384e78f8a
Mark Feldhousen <markf@geekpad.com>
2018-08-17 10:12:57 -0400

    remove unused origin clause

--- terraform_egress_pub/cloudfront.tf
+++ terraform_egress_pub/cloudfront.tf
@@ -6,10 +6,6 @@ resource "aws_cloudfront_distribution" "rules_s3_distribution" {
   origin {
     domain_name = "${aws_s3_bucket.rules_bucket.bucket_regional_domain_name}"
     origin_id   = "${local.s3_origin_id}"
-
-    # s3_origin_config {
-    #   origin_access_identity = ""
-    # }
   }
 
   enabled             = true
