e2b602d4ce2440acda948bafca2a7fcf428e7884
Mark Feldhousen <markf@geekpad.com>
2018-08-17 18:19:58 -0400

    add multiple nmap instances with count calculate sequential ip addresses within scanner subnet disable ansible provisioner as it assumes one nmap instance TODO: fix ansible provisioner once I understand this voodoo

--- terraform/cyhy_nmap_ec2.tf
+++ terraform/cyhy_nmap_ec2.tf
@@ -23,10 +23,14 @@ data "aws_ami" "nmap" {
 resource "aws_instance" "cyhy_nmap" {
   ami = "${data.aws_ami.nmap.id}"
   instance_type = "t2.micro"
+  count = "${terraform.workspace == "production" ? 4 : 2}"
+
   # ebs_optimized = true
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
 
   subnet_id = "${aws_subnet.cyhy_scanner_subnet.id}"
+  private_ip = "${cidrhost(aws_subnet.cyhy_scanner_subnet.cidr_block, count.index+1)}"
+
   associate_public_ip_address = true
 
   root_block_device {
@@ -46,18 +50,18 @@ resource "aws_instance" "cyhy_nmap" {
 }
 
 # Provision the nmap EC2 instance via Ansible
-module "cyhy_nmap_ansible_provisioner" {
-  source = "github.com/cloudposse/tf_ansible"
-
-  arguments = [
-    "--user=${var.remote_ssh_user}",
-    "--ssh-common-args='-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -W %h:%p -o StrictHostKeyChecking=no -q ${var.remote_ssh_user}@${aws_instance.cyhy_bastion.public_ip}\"'"
-  ]
-  envs = [
-    "host=${aws_instance.cyhy_nmap.private_ip}",
-    "bastion_host=${aws_instance.cyhy_bastion.public_ip}",
-    "host_groups=cyhy_runner"
-  ]
-  playbook = "../ansible/playbook.yml"
-  dry_run = false
-}
+# module "cyhy_nmap_ansible_provisioner" {
+#   source = "github.com/cloudposse/tf_ansible"
+#
+#   arguments = [
+#     "--user=${var.remote_ssh_user}",
+#     "--ssh-common-args='-o StrictHostKeyChecking=no -o ProxyCommand=\"ssh -W %h:%p -o StrictHostKeyChecking=no -q ${var.remote_ssh_user}@${aws_instance.cyhy_bastion.public_ip}\"'"
+#   ]
+#   envs = [
+#     "host=${aws_instance.cyhy_nmap.private_ip}",
+#     "bastion_host=${aws_instance.cyhy_bastion.public_ip}",
+#     "host_groups=cyhy_runner"
+#   ]
+#   playbook = "../ansible/playbook.yml"
+#   dry_run = false
+# }
