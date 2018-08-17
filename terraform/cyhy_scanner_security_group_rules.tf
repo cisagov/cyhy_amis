ad43f4cdee11a8e65c688344843f965ab748b4c2
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-08-17 09:25:14 -0400

    Break the bastion into its own security group

--- terraform/cyhy_scanner_security_group_rules.tf
+++ terraform/cyhy_scanner_security_group_rules.tf
@@ -1,30 +1,14 @@
-# Allow ingress from trusted networks via the Nessus UI and ssh ports
-resource "aws_security_group_rule" "scanner_ingress_from_trusted" {
+# Allow ingress from the bastion security group via the ssh and Nessus
+# ports
+resource "aws_security_group_rule" "scanner_ingress_from_bastion_sg" {
   count = "${length(local.cyhy_trusted_ingress_ports)}"
-
+  
   security_group_id = "${aws_security_group.cyhy_scanner_sg.id}"
   type = "ingress"
   protocol = "tcp"
-  cidr_blocks = "${var.trusted_ingress_networks_ipv4}"
-  # ipv6_cidr_blocks = "${var.trusted_ingress_networks_ipv6}"
+  source_security_group_id = "${aws_security_group.cyhy_bastion_sg.id}"
   from_port = "${local.cyhy_trusted_ingress_ports[count.index]}"
   to_port = "${local.cyhy_trusted_ingress_ports[count.index]}"
-}
-
-# Allow ingress from the bastion via ssh
-#
-# We have to include the public IP because Ansible uses the ssh proxy
-# even when connecting to the bastion.
-resource "aws_security_group_rule" "scanner_ingress_from_bastion_via_ssh" {
-  security_group_id = "${aws_security_group.cyhy_scanner_sg.id}"
-  type = "ingress"
-  protocol = "tcp"
-  cidr_blocks = [
-    "${aws_instance.cyhy_bastion.public_ip}/32",
-    "${aws_instance.cyhy_bastion.private_ip}/32"
-  ]
-  from_port = 22
-  to_port = 22
 }
 
 # Allow ingress via ssh from the private security group
