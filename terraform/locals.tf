d624a08e8f135ead0621c88788698970f2c48700
Mark Feldhousen <markf@geekpad.com>
2018-08-20 18:17:37 -0400

    add comments for dns caculations tie dns record creation to instance counts (DNS takes a long time)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -6,7 +6,7 @@ locals {
   # TODO no dynamic workspace until we can loop modules (see below)
   nmap_instance_count = "2"   #"${local.production_workspace ? 32 : 1}"
   nessus_instance_count = "2" #"${local.production_workspace ? 4 : 1}"
-  mongo_instance_count = "1"
+  mongo_instance_count = "1"  # TODO: stuck at one until we can scale mongo_ec2
 
   # These are the ports via which trusted networks are allowed to
   # access the public-facing CyHy hosts
@@ -21,14 +21,24 @@ locals {
     "udp"
   ]
 
-  # divy up the subnets
-  # NOTE: there is an assumption that they are /24
+  # DNS zone calculations based on requested instances
+  # The numbers represent the count of IP addresses in a subnet
+  # and are used by the cidrhost() function.
+  # NOTE: there is an assumption that subnets are /24 in the reverse zone names
+
+  # Port Scanners DNS entries
   first_port_scanner = 11
-  count_port_scanner = 64
+  count_port_scanner = "${local.nmap_instance_count}"
+
+  # Vulnerability Scanners DNS entries
   first_vuln_scanner = 201
-  count_vuln_scanner = 8
+  count_vuln_scanner = "${local.nessus_instance_count}"
+
+  # Database DNS entries
   first_database = 11
-  count_database = 4
+  count_database = "${local.mongo_instance_count}"
+
+  # Singleton DNS entries
   the_commander = 5 # there can be only one
   the_bastion = 254 # there can be only one
 }
