810defb823a4e16c60d89c2b7694e3eeecfe1d97
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-09-27 14:21:07 -0400

    Add line for jsf9k workspace

--- terraform/configure.py
+++ terraform/configure.py
@@ -28,6 +28,7 @@ assert sys.version_info >= (3,7), 'This script requires Python version 3.7 or ne
 WORKSPACE_CONFIGS = {
                         'production':   {'nmap':64, 'nessus':3, 'mongo':1},
                         'felddy':       {'nmap':4, 'nessus':1, 'mongo':1},
+                        'jsf9k':        {'nmap':0, 'nessus':0, 'mongo':1},
                     }
 
 # the default configuration if a workspace is not defined above
