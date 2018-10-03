f607d6078bb3cf20434001bd5c90dd405df22a5b
Dave Redmin <david.redmin@beta.dhs.gov>
2018-10-02 22:15:37 -0400

    Update scanner subnet ACL rules to reflect addition of public subnet and to allow ssh ingress from private subnet so that the commander can talk to the scanners

--- terraform/cyhy_scanner_acl_rules.tf
+++ terraform/cyhy_scanner_acl_rules.tf
@@ -1,5 +1,5 @@
-# Allow ingress from anywhere via the Nessus UI and ssh ports
-resource "aws_network_acl_rule" "scanner_ingress_from_anywhere_via_nessus_and_ssh" {
+# Allow ingress from public subnet (NAT gateway) via the Nessus UI and ssh ports
+resource "aws_network_acl_rule" "scanner_ingress_from_public_via_nessus_and_ssh" {
   count = "${length(local.cyhy_trusted_ingress_ports)}"
 
   network_acl_id = "${aws_network_acl.cyhy_scanner_acl.id}"
@@ -7,7 +7,7 @@ resource "aws_network_acl_rule" "scanner_ingress_from_anywhere_via_nessus_and_ss
   protocol = "tcp"
   rule_number = "${100 + count.index}"
   rule_action = "allow"
-  cidr_block = "0.0.0.0/0"
+  cidr_block = "${aws_subnet.cyhy_public_subnet.cidr_block}"
   from_port = "${local.cyhy_trusted_ingress_ports[count.index]}"
   to_port = "${local.cyhy_trusted_ingress_ports[count.index]}"
 }
@@ -39,28 +39,27 @@ resource "aws_network_acl_rule" "scanner_ingress_from_anywhere_via_icmp" {
   icmp_code = -1
 }
 
+# Allow ssh ingress from private subnet, needed for commander ssh to scanners
+resource "aws_network_acl_rule" "scanner_ingress_from_private_via_ssh" {
+  network_acl_id = "${aws_network_acl.cyhy_scanner_acl.id}"
+  egress = false
+  protocol = "tcp"
+  rule_number = 140
+  rule_action = "allow"
+  cidr_block = "${aws_subnet.cyhy_private_subnet.cidr_block}"
+  from_port = 22
+  to_port = 22
+}
+
 # Allow egress to anywhere via any protocol and port, since we're
 # scanning
 resource "aws_network_acl_rule" "scanner_egress_to_anywhere_via_any_port" {
   network_acl_id = "${aws_network_acl.cyhy_scanner_acl.id}"
   egress = true
   protocol = "-1"
-  rule_number = 140
+  rule_number = 150
   rule_action = "allow"
   cidr_block = "0.0.0.0/0"
   from_port = 0
   to_port = 0
-}
-
-# Allow https ingress from private subnet, needed for outbound https from
-# private subnet, which goes through the NAT GW (resides in scanner subnet)
-resource "aws_network_acl_rule" "scanner_ingress_from_private_via_https" {
-  network_acl_id = "${aws_network_acl.cyhy_scanner_acl.id}"
-  egress = false
-  protocol = "tcp"
-  rule_number = 150
-  rule_action = "allow"
-  cidr_block = "${aws_subnet.cyhy_private_subnet.cidr_block}"
-  from_port = 443
-  to_port = 443
 }
