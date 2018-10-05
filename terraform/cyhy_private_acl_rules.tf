251056cd70a476b9944bf417b2182a99c1951de5
Dave Redmin <david.redmin@beta.dhs.gov>
2018-10-04 23:30:55 -0400

    Update ACL rules for private subnet to include both scanner subnets

--- terraform/cyhy_private_acl_rules.tf
+++ terraform/cyhy_private_acl_rules.tf
@@ -1,11 +1,22 @@
-# Allow egress to the scanner subnet via ssh
-resource "aws_network_acl_rule" "private_egress_to_scanner_via_ssh" {
+# Allow egress to the both scanner subnets via ssh
+resource "aws_network_acl_rule" "private_egress_to_portscanner_via_ssh" {
   network_acl_id = "${aws_network_acl.cyhy_private_acl.id}"
   egress = true
   protocol = "tcp"
   rule_number = 100
   rule_action = "allow"
-  cidr_block = "${aws_subnet.cyhy_scanner_subnet.cidr_block}"
+  cidr_block = "${aws_subnet.cyhy_portscanner_subnet.cidr_block}"
+  from_port = 22
+  to_port = 22
+}
+
+resource "aws_network_acl_rule" "private_egress_to_vulnscanner_via_ssh" {
+  network_acl_id = "${aws_network_acl.cyhy_private_acl.id}"
+  egress = true
+  protocol = "tcp"
+  rule_number = 101
+  rule_action = "allow"
+  cidr_block = "${aws_subnet.cyhy_vulnscanner_subnet.cidr_block}"
   from_port = 22
   to_port = 22
 }
@@ -15,7 +26,7 @@ resource "aws_network_acl_rule" "private_egress_to_mongo_via_mongo" {
   network_acl_id = "${aws_network_acl.cyhy_private_acl.id}"
   egress = true
   protocol = "tcp"
-  rule_number = 101
+  rule_number = 102
   rule_action = "allow"
   cidr_block = "${aws_instance.cyhy_mongo.private_ip}/32"
   from_port = 27017
@@ -28,7 +39,7 @@ resource "aws_network_acl_rule" "cyhy_private_egress_anywhere_via_https" {
   network_acl_id = "${aws_network_acl.cyhy_private_acl.id}"
   egress = true
   protocol = "tcp"
-  rule_number = 102
+  rule_number = 103
   rule_action = "allow"
   cidr_block = "0.0.0.0/0"
   from_port = 443
@@ -41,7 +52,7 @@ resource "aws_network_acl_rule" "private_ingress_from_anywhere_via_ephemeral_por
   network_acl_id = "${aws_network_acl.cyhy_private_acl.id}"
   egress = false
   protocol = "tcp"
-  rule_number = 103
+  rule_number = 104
   rule_action = "allow"
   cidr_block = "0.0.0.0/0"
   from_port = 1024
