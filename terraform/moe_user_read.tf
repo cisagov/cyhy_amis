1196d59e258306f4451826503eb2bcb5226639a9
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-10-19 11:24:43 -0400

    Make user policies standalone instead of inline
    Also:
    * Remove the users from the /system path, since we don't yet have an
      organizational scheme for users.
    * Append the Terraform workspace name to the user names to avoid
      conflicts.

--- terraform/moe_user_read.tf
+++ terraform/moe_user_read.tf
@@ -1,39 +1,41 @@
-resource "aws_iam_user" "create_moe_user_read" {
-  name = "moe_user_read"
-  path = "/system/"
+resource "aws_iam_user" "moe_user_read" {
+  name = "moe_user_read_${terraform.workspace}"
 }
 
-resource "aws_iam_access_key" "create_moe_user_read" {
-  user = "${aws_iam_user.create_moe_user_read.name}"
+resource "aws_iam_access_key" "moe_user_read" {
+  user = "${aws_iam_user.moe_user_read.name}"
 }
 
-resource "aws_iam_user_policy" "create_moe_user_read" {
-  name = "moe_bucket_read_access"
-  user = "${aws_iam_user.create_moe_user_read.name}"
+# IAM policy document that that allows read permissions on the MOE
+# bucket.  This will be applied to the moe_user_read role.
+data "aws_iam_policy_document" "moe_read_doc" {
+  statement {
+    effect = "Allow"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:ListBucket"
-            ],
-            "Resource": [
-                "arn:aws:s3:::ncats-moe-data"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::ncats-moe-data/*"
-            ]
-        }
+    actions = [
+      "s3:ListBucket"
     ]
+
+    resources = [
+      "arn:aws:s3:::ncats-moe-data"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::ncats-moe-data/*"
+    ]
+  }
 }
-EOF
+
+# The S3 policy for our role
+resource "aws_iam_user_policy" "moe_read_policy" {
+  user = "${aws_iam_user.moe_user_read.name}"
+  policy = "${data.aws_iam_policy_document.moe_read_doc.json}"
 }
