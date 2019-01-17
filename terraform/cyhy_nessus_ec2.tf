0db84b7a1782032b8a34d6d54822ac3cf7334312
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2019-01-16 23:00:29 -0500

    Add a note stating why we can't upgrade to an NVMe instance

--- terraform/cyhy_nessus_ec2.tf
+++ terraform/cyhy_nessus_ec2.tf
@@ -22,6 +22,8 @@ data "aws_ami" "nessus" {
 
 resource "aws_instance" "cyhy_nessus" {
   ami = "${data.aws_ami.nessus.id}"
+  # The base Nessus AMI won't support NVMe devices, so we can't
+  # upgrade to m5 instances :(
   instance_type = "${local.production_workspace ? "m4.2xlarge" : "m4.large"}"
   count = "${local.nessus_instance_count}"
   availability_zone = "${var.aws_region}${var.aws_availability_zone}"
