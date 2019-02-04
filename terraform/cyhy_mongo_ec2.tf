68d23fa472dae8cfc5757c5e5ea82df86d202925
Kyle Evers <kyleevers14@yahoo.com>
2019-02-04 13:00:19 -0500

    set feeds cronjob to only run in production

--- terraform/cyhy_mongo_ec2.tf
+++ terraform/cyhy_mongo_ec2.tf
@@ -65,7 +65,8 @@ module "cyhy_mongo_ansible_provisioner" {
     "bastion_host=${aws_instance.cyhy_bastion.public_ip}",
     "cyhy_archive_s3_bucket_name=${aws_s3_bucket.cyhy_archive.bucket}",
     "cyhy_archive_s3_bucket_region=${var.aws_region}",
-    "host_groups=mongo,cyhy_commander,cyhy_archive"
+    "host_groups=mongo,cyhy_commander,cyhy_archive",
+    "production_workspace=${local.production_workspace}"
   ]
   playbook = "../ansible/playbook.yml"
   dry_run = false
