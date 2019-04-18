20d7f4884cba114fe44718e8b502a4e42fb8c711
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-04-18 14:25:00 -0400

    Bug fix for set_hostname.sh script
    In my testing I found that sometimes at boot the host will resolve,
    then briefly fail to resolve while AWS networking settles into place.
    This code change handles that case.

--- terraform/scripts/set_hostname.sh
+++ terraform/scripts/set_hostname.sh
@@ -8,14 +8,18 @@ set -o pipefail
 
 ip_addr=$(hostname --all-ip-addresses | cut --delimiter=' ' --fields=1)
 
-while ! host "$ip_addr"
+# The return value of variable=$(command) is the return value of the
+# command.  We need this because sometimes at boot the host will
+# resolve, then briefly fail to resolve while AWS networking settles
+# into place.
+while ! hostinfo=$(host "$ip_addr")
 do
     echo Waiting for IP to resolve to an address...
     sleep 5
 done
 
-name=$(host "$ip_addr" | \
+name=$(echo "$hostinfo" | \
            cut --delimiter=' ' --fields=5 | \
            sed 's/\(.*\)\.local\./\1/')
 
-hostnamectl set-hostname "$name"
+hostnamectl set-hostname --no-ask-password "$name"
