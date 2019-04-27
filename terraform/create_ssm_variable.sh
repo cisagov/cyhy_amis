129d99043d8777281c8d9dd7504130eb1cb9b83e
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-04-27 12:35:52 -0400

    Add a script to create AWS SSM variables in all four US regions

---
+++ terraform/create_ssm_variable.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# create_ssm_variable.sh [args]
+#
+# Note that the args are precisely the arguments that would be passed
+# to the command aws ssm put-parameter.
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+for region in us-east-1 us-east-2 us-west-1 us-west-2
+do
+    aws --region=$region ssm put-parameter "$@"
+done
