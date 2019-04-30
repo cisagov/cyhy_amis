28e59470f265f7cf433fb32d88cb9e3d8ba9ae2e
Dave Redmin <david.redmin@beta.dhs.gov>
2019-04-29 22:50:34 -0400

    Add rule in the adi lambda security group to allow HTTPS egress to anywhere so that boto3 S3/SSM requests by the lambda can egress

--- terraform/adi_lambda_security_group_rules.tf
+++ terraform/adi_lambda_security_group_rules.tf
@@ -9,3 +9,13 @@ resource "aws_security_group_rule" "adi_lambda_to_cyhy_mongo" {
   from_port = "27017"
   to_port = "27017"
 }
+
+# Allow HTTPS egress anywhere; needed to access AWS S3 bucket via boto3
+resource "aws_security_group_rule" "adi_lambda_https_egress_anywhere" {
+  security_group_id = "${aws_security_group.adi_lambda_sg.id}"
+  type = "egress"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port = 443
+  to_port = 443
+}
