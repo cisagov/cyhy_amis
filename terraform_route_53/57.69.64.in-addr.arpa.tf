e6811de8cf0ad8893f5fe933cc5bfc79de79d57b
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-06-06 13:56:34 -0400

    Run terraform 0.12upgrade on Route53 code

--- terraform_route_53/57.69.64.in-addr.arpa.tf
+++ terraform_route_53/57.69.64.in-addr.arpa.tf
@@ -11,33 +11,34 @@ resource "aws_route53_zone" "reverse_zone" {
 # See: https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/SOA-NSrecords.html
 
 resource "aws_route53_record" "rev_1_PTR" {
-  zone_id = "${aws_route53_zone.reverse_zone.zone_id}"
+  zone_id = aws_route53_zone.reverse_zone.zone_id
   name    = "1.${aws_route53_zone.reverse_zone.name}"
   type    = "PTR"
   ttl     = 300
-  records = [ "router.ncats.cyber.dhs.gov." ]
+  records = ["router.ncats.cyber.dhs.gov."]
 }
 
 resource "aws_route53_record" "rev_2_PTR" {
-  zone_id = "${aws_route53_zone.reverse_zone.zone_id}"
+  zone_id = aws_route53_zone.reverse_zone.zone_id
   name    = "2.${aws_route53_zone.reverse_zone.name}"
   type    = "PTR"
   ttl     = 300
-  records = [ "vip.ncats.cyber.dhs.gov." ]
+  records = ["vip.ncats.cyber.dhs.gov."]
 }
 
 resource "aws_route53_record" "rev_3_PTR" {
-  zone_id = "${aws_route53_zone.reverse_zone.zone_id}"
+  zone_id = aws_route53_zone.reverse_zone.zone_id
   name    = "3.${aws_route53_zone.reverse_zone.name}"
   type    = "PTR"
   ttl     = 300
-  records = [ "fw01.ncats.cyber.dhs.gov." ]
+  records = ["fw01.ncats.cyber.dhs.gov."]
 }
 
 resource "aws_route53_record" "rev_4_PTR" {
-  zone_id = "${aws_route53_zone.reverse_zone.zone_id}"
+  zone_id = aws_route53_zone.reverse_zone.zone_id
   name    = "4.${aws_route53_zone.reverse_zone.name}"
   type    = "PTR"
   ttl     = 300
-  records = [ "fw02.ncats.cyber.dhs.gov." ]
+  records = ["fw02.ncats.cyber.dhs.gov."]
 }
+
--- terraform_route_53/cyber.dhs.gov.tf
+++ terraform_route_53/cyber.dhs.gov.tf
@@ -7,8 +7,8 @@ resource "aws_route53_zone" "cyber_zone" {
 }
 
 resource "aws_route53_record" "root_A" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
-  name    = "${aws_route53_zone.cyber_zone.name}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
+  name    = aws_route53_zone.cyber_zone.name
   type    = "A"
 
   alias {
@@ -19,8 +19,8 @@ resource "aws_route53_record" "root_A" {
 }
 
 resource "aws_route53_record" "root_AAAA" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
-  name    = "${aws_route53_zone.cyber_zone.name}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
+  name    = aws_route53_zone.cyber_zone.name
   type    = "AAAA"
 
   alias {
@@ -31,35 +31,36 @@ resource "aws_route53_record" "root_AAAA" {
 }
 
 resource "aws_route53_record" "root_CAA" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
-  name    = "${aws_route53_zone.cyber_zone.name}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
+  name    = aws_route53_zone.cyber_zone.name
   type    = "CAA"
   ttl     = 300
-  records = [ "0 issue \"letsencrypt.org\"",
-              "0 issue \"amazon.com\"",
-              "0 issuewild \";\"",
-              "0 iodef \"mailto:NCATSSecurity@hq.dhs.gov\""
-            ]
+  records = [
+    "0 issue \"letsencrypt.org\"",
+    "0 issue \"amazon.com\"",
+    "0 issuewild \";\"",
+    "0 iodef \"mailto:NCATSSecurity@hq.dhs.gov\"",
+  ]
 }
 
 # This DNS record gives Amazon Certificate Manager permission to
 # generate certificates for rules.ncats.cyber.dhs.gov
 resource "aws_route53_record" "root_acm_rules_CNAME" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
-  name = "_724d852f42d6b10ed1c6ab4135301ef6.rules.ncats.${aws_route53_zone.cyber_zone.name}"
-  type = "CNAME"
-  ttl = 60
+  zone_id = aws_route53_zone.cyber_zone.zone_id
+  name    = "_724d852f42d6b10ed1c6ab4135301ef6.rules.ncats.${aws_route53_zone.cyber_zone.name}"
+  type    = "CNAME"
+  ttl     = 60
   records = [
-    "_548e9cb4a195b3c2a5410a9ff88fcda3.acm-validations.aws"
+    "_548e9cb4a195b3c2a5410a9ff88fcda3.acm-validations.aws",
   ]
 }
 
 resource "aws_route53_record" "root_MX" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
-  name    = "${aws_route53_zone.cyber_zone.name}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
+  name    = aws_route53_zone.cyber_zone.name
   type    = "MX"
   ttl     = 300
-  records = [ "10 inbound-smtp.us-east-1.amazonaws.com" ]
+  records = ["10 inbound-smtp.us-east-1.amazonaws.com"]
 }
 
 # NS and SOA records are assigned at zone creation and should not be modified
@@ -87,127 +88,127 @@ resource "aws_route53_record" "root_MX" {
 # }
 
 resource "aws_route53_record" "root_SPF" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
-  name    = "${aws_route53_zone.cyber_zone.name}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
+  name    = aws_route53_zone.cyber_zone.name
   type    = "TXT"
   ttl     = 300
-  records = [ "v=spf1 include:amazonses.com -all" ]
+  records = ["v=spf1 include:amazonses.com -all"]
 }
 
 resource "aws_route53_record" "_amazonses_TXT" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "_amazonses.${aws_route53_zone.cyber_zone.name}"
   type    = "TXT"
   ttl     = 60
-  records = [ "tOxXTap6jGLn6/VnBnget7lrXW+TxZTyTdOhm8LbM/Y=" ]
+  records = ["tOxXTap6jGLn6/VnBnget7lrXW+TxZTyTdOhm8LbM/Y="]
 }
 
 resource "aws_route53_record" "_dmarc_TXT" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "_dmarc.${aws_route53_zone.cyber_zone.name}"
   type    = "TXT"
   ttl     = 1800
-  records = [ "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=r; rua=mailto:reports@dmarc.cyber.dhs.gov; rf=afrf; pct=100; ri=86400" ]
+  records = ["v=DMARC1; p=reject; sp=reject; adkim=s; aspf=r; rua=mailto:reports@dmarc.cyber.dhs.gov; rf=afrf; pct=100; ri=86400"]
 }
 
 resource "aws_route53_record" "dmarc_MX" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "dmarc.${aws_route53_zone.cyber_zone.name}"
   type    = "MX"
   ttl     = 1800
-  records = [ "10 inbound-smtp.us-east-1.amazonaws.com" ]
+  records = ["10 inbound-smtp.us-east-1.amazonaws.com"]
 }
 
 resource "aws_route53_record" "_amazonses_dmarc_TXT" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "_amazonses.dmarc.${aws_route53_zone.cyber_zone.name}"
   type    = "TXT"
   ttl     = 60
-  records = [ "CV4Ex6gYlJutTAnA8xkQa0hk3toSRuFvmibJ0sRiAWw=" ]
+  records = ["CV4Ex6gYlJutTAnA8xkQa0hk3toSRuFvmibJ0sRiAWw="]
 }
 
 resource "aws_route53_record" "wildcard_report_dmarc_TXT" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "*._report._dmarc.dmarc.${aws_route53_zone.cyber_zone.name}"
   type    = "TXT"
   ttl     = 300
-  records = [ "v=DMARC1" ]
+  records = ["v=DMARC1"]
 }
 
 resource "aws_route53_record" "dkim1_dmarc_CNAME" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "6na6lcj7onl5bco4ytfj4ud7p6t7kvtp._domainkey.dmarc.${aws_route53_zone.cyber_zone.name}"
   type    = "CNAME"
   ttl     = 1800
-  records = [ "6na6lcj7onl5bco4ytfj4ud7p6t7kvtp.dkim.amazonses.com" ]
+  records = ["6na6lcj7onl5bco4ytfj4ud7p6t7kvtp.dkim.amazonses.com"]
 }
 
 resource "aws_route53_record" "dkim2_dmarc_CNAME" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "nsbndtrubsyckjqnb4wkv6xdkrqe3dk5._domainkey.dmarc.${aws_route53_zone.cyber_zone.name}"
   type    = "CNAME"
   ttl     = 1800
-  records = [ "nsbndtrubsyckjqnb4wkv6xdkrqe3dk5.dkim.amazonses.com" ]
+  records = ["nsbndtrubsyckjqnb4wkv6xdkrqe3dk5.dkim.amazonses.com"]
 }
 
 resource "aws_route53_record" "dkim3_dmarc_CNAME" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "yhfkaco3ukhtbowt2bdvfz5czwuofitm._domainkey.dmarc.${aws_route53_zone.cyber_zone.name}"
   type    = "CNAME"
   ttl     = 1800
-  records = [ "yhfkaco3ukhtbowt2bdvfz5czwuofitm.dkim.amazonses.com" ]
+  records = ["yhfkaco3ukhtbowt2bdvfz5czwuofitm.dkim.amazonses.com"]
 }
 
 resource "aws_route53_record" "drop_ncats_A" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "drop.ncats.${aws_route53_zone.cyber_zone.name}"
   type    = "A"
   ttl     = 60
-  records = [ "64.69.57.40" ]
+  records = ["64.69.57.40"]
 }
 
 resource "aws_route53_record" "fw01_ncats_A" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "fw01.ncats.${aws_route53_zone.cyber_zone.name}"
   type    = "A"
   ttl     = 300
-  records = [ "64.69.57.3" ]
+  records = ["64.69.57.3"]
 }
 
 resource "aws_route53_record" "fw02_ncats_A" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "fw02.ncats.${aws_route53_zone.cyber_zone.name}"
   type    = "A"
   ttl     = 300
-  records = [ "64.69.57.4" ]
+  records = ["64.69.57.4"]
 }
 
 resource "aws_route53_record" "vip_ncats_A" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "vip.ncats.${aws_route53_zone.cyber_zone.name}"
   type    = "A"
   ttl     = 300
-  records = [ "64.69.57.2" ]
+  records = ["64.69.57.2"]
 }
 
 resource "aws_route53_record" "vpn_ncats_CNAME" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "vpn.ncats.${aws_route53_zone.cyber_zone.name}"
   type    = "CNAME"
   ttl     = 300
-  records = [ "vip.ncats.cyber.dhs.gov" ]
+  records = ["vip.ncats.cyber.dhs.gov"]
 }
 
 resource "aws_route53_record" "rr_CNAME" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "rr.${aws_route53_zone.cyber_zone.name}"
   type    = "CNAME"
   ttl     = 60
-  records = [ "rr.cyber.dhs.gov.s3-website-us-east-1.amazonaws.com" ]
+  records = ["rr.cyber.dhs.gov.s3-website-us-east-1.amazonaws.com"]
 }
 
 resource "aws_route53_record" "rules_ncats_A" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "rules.ncats.${aws_route53_zone.cyber_zone.name}"
   type    = "A"
 
@@ -219,7 +220,7 @@ resource "aws_route53_record" "rules_ncats_A" {
 }
 
 resource "aws_route53_record" "rules_ncats_AAAA" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "rules.ncats.${aws_route53_zone.cyber_zone.name}"
   type    = "AAAA"
 
@@ -231,17 +232,18 @@ resource "aws_route53_record" "rules_ncats_AAAA" {
 }
 
 resource "aws_route53_record" "mail_MX" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "mail.${aws_route53_zone.cyber_zone.name}"
   type    = "MX"
   ttl     = 300
-  records = [ "10 feedback-smtp.us-east-1.amazonses.com" ]
+  records = ["10 feedback-smtp.us-east-1.amazonses.com"]
 }
 
 resource "aws_route53_record" "mail_SPF" {
-  zone_id = "${aws_route53_zone.cyber_zone.zone_id}"
+  zone_id = aws_route53_zone.cyber_zone.zone_id
   name    = "mail.${aws_route53_zone.cyber_zone.name}"
   type    = "TXT"
   ttl     = 300
-  records = [ "v=spf1 include:amazonses.com -all" ]
+  records = ["v=spf1 include:amazonses.com -all"]
 }
+
