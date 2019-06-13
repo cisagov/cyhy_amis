3d58b933a040d74c1cf7dc99fb76f1181486f02b
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-13 13:19:42 -0400

    Add new variables needed for manual scan Nessus

--- terraform_nessus_only/variables.tf
+++ terraform_nessus_only/variables.tf
@@ -33,3 +33,28 @@ variable "trusted_ingress_networks_ipv6" {
   description = "Trusted IPv6 ingress networks"
 }
 
+variable "remote_ssh_user" {
+  description = "The username to use when sshing to the EC2 instances"
+}
+
+variable "nessus_instance_count" {
+  type        = number
+  description = "The number of Nessus instances to create."
+  default     = 1
+}
+
+variable "nessus_activation_codes" {
+  type        = list(string)
+  description = "A list of strings containing Nessus activation codes"
+}
+
+variable "cyhy_elastic_ip_cidr_block" {
+  description = "The CIDR block of elastic addresses available for use by CyHy scanner instances.  This is only used in Production workspaces."
+  default     = ""
+}
+
+variable "nessus_first_elastic_ip_offset" {
+  type        = number
+  description = "The offset of the address (from the start of the elastic IP CIDR block) to be assigned to the *first* Nessus instance.  For example, if the CIDR block is 192.168.1.0/24 and the offset is set to 10, the first Nessus address used will be 192.168.1.10.  This is only used in production workspaces.  Each additional Nessus instance will get the next consecutive address in the block.  NOTE: This will only work as intended when a contiguous CIDR block of EIP addresses is available.  This is only used in Production workspaces."
+  default     = 1
+}
