a404dfe25c0c9b652be705f952a838bd45c72de8
Dave Redmin <david.redmin@beta.dhs.gov>
2019-12-05 14:08:56 -0500

    Update syntax to remove terraform validation warnings ("Quoted type constraints are deprecated")

--- terraform/dyn_mgmt_nessus/variables.tf
+++ terraform/dyn_mgmt_nessus/variables.tf
@@ -1,4 +1,4 @@
 variable "mgmt_bastion_public_ip" {}
-variable "mgmt_nessus_private_ips" { type = "list" }
-variable "mgmt_nessus_activation_codes" { type = "list" }
+variable "mgmt_nessus_private_ips" { type = list(string) }
+variable "mgmt_nessus_activation_codes" { type = list(string) }
 variable "remote_ssh_user" {}
--- terraform_nessus_only/dyn_nessus/variables.tf
+++ terraform_nessus_only/dyn_nessus/variables.tf
@@ -1,3 +1,3 @@
-variable "nessus_public_ips" { type = "list" }
-variable "nessus_activation_codes" { type = "list" }
+variable "nessus_public_ips" { type = list(string) }
+variable "nessus_activation_codes" { type = list(string) }
 variable "remote_ssh_user" {}
