6ee5d6ac9f641723976b19fdf4327ff5cb7967df
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-01-27 14:16:46 -0500

    Add script to deploy a new findings-data-import lambda version. Switch the Python environment from python3.6 to python3.8 to mirror changes made in the lambda's codebase.

---
+++ terraform/deploy_new_fdi_lambda.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# deploy_new_fdi_lambda.sh workspace_name
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+if [ $# -eq 1 ]
+then
+    workspace=$1
+else
+    echo "Usage:  deploy_new_fdi_lambda.sh workspace_name"
+    exit 1
+fi
+
+terraform workspace select "$workspace"
+
+# taint the existing lambda instance
+terraform taint "aws_lambda_function.fdi_lambda"
+
+# recreate the new lambda instance
+terraform apply -var-file="$workspace.tfvars" \
+          -target=aws_lambda_function.fdi_lambda \
+          -target=aws_lambda_permission.fdi_lambda_allow_bucket \
+          -target=aws_s3_bucket_notification.bucket_notification \
+          -target=aws_cloudwatch_log_group.fdi_lambda_logs
+          
