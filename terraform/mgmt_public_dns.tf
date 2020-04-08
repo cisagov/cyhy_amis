6156f9570516a6a050301651fb1b3105a4c81f63
Dave Redmin <david.redmin@beta.dhs.gov>
2020-04-08 11:11:32 -0400

    Use new DNS provider to manage A records for BOD, CyHy, and Management bastion instances.

--- terraform/mgmt_public_dns.tf
+++ terraform/mgmt_public_dns.tf
@@ -1,16 +1,12 @@
-data "aws_route53_zone" "mgmt_public_zone" {
-  count = var.enable_mgmt_vpc ? 1 : 0
-
-  name = local.mgmt_public_zone
-}
-
 resource "aws_route53_record" "mgmt_bastion_pub_A" {
+  provider = aws.public_dns
   count = var.enable_mgmt_vpc ? 1 : 0
 
-  zone_id = data.aws_route53_zone.mgmt_public_zone[0].zone_id
-  name    = "bastion.${terraform.workspace}.${local.mgmt_public_subdomain}${data.aws_route53_zone.mgmt_public_zone[0].name}"
+  zone_id = data.terraform_remote_state.dns.outputs.cyber_dhs_gov_zone.id
+  name    = "bastion.${terraform.workspace}.${local.mgmt_public_subdomain}${data.terraform_remote_state.dns.outputs.cyber_dhs_gov_zone.name}"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.mgmt_bastion[0].public_ip]
+  records = [
+    aws_instance.mgmt_bastion[0].public_ip,
+  ]
 }
-
