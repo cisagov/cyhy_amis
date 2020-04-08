2cccd9ac0ae82798cf2148ec652dadd3e0cf1c63
Dave Redmin <david.redmin@beta.dhs.gov>
2020-04-08 11:07:58 -0400

    Add provider that can make changes to DNS entries in the public cyber.dhs.gov zone.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,18 @@ provider "aws" {
   region = var.aws_region
 }
 
+# This is the provider that can make changes to DNS entries
+# in the public cyber.dhs.gov zone.
+#
+# NOTE: After the CyHy AWS account moves in to the COOL, it will be possible
+# to assume the role below via Terraform remote state.  For details see:
+# https://github.com/cisagov/pca-teamserver-aws/pull/30#discussion_r400610194
+provider "aws" {
+  alias = "public_dns"
+  profile = "cool-dns-route53resourcechange-cyber.dhs.gov"
+  region = var.aws_region
+}
+
 # The AWS account ID being used
 data "aws_caller_identity" "current" {
 }
@@ -10,4 +22,3 @@ data "aws_caller_identity" "current" {
 # The list of available AWS availability zones
 data "aws_availability_zones" "all" {
 }
-
