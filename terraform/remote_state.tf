b19d0c0170747e86802042b0e4c4293e6445df57
Dave Redmin <david.redmin@beta.dhs.gov>
2020-04-08 11:08:56 -0400

    Add remote state that contains details about the public DNS zone for cyber.dhs.gov.

---
+++ terraform/remote_state.tf
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------
+# Retrieves state data from a Terraform backend. This allows use of the
+# root-level outputs of one or more Terraform configurations as input data
+# for this configuration.
+# ------------------------------------------------------------------------------
+
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-readstate"
+    region         = "us-east-1"
+    key            = "cool-dns-cyber.dhs.gov.tfstate"
+  }
+
+  workspace = "production"
+}
