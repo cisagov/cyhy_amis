6d69c56583e95b4fa9f88e61006669d253000d98
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-06-17 14:58:50 -0400

    Update the portscan deployment script to support multiple sequential instances.
    Tweak the script to allow an alternative call that supports a starting and
    ending index for an instance range.

--- terraform/deploy_new_portscan_instance.sh
+++ terraform/deploy_new_portscan_instance.sh
@@ -1,40 +1,62 @@
 #!/usr/bin/env bash
 
 # deploy_new_portscan_instance.sh region workspace_name instance_index
+# deploy_new_portscan_instance.sh region workspace_name first_instance last_instance
 
 set -o nounset
 set -o errexit
 set -o pipefail
 
+function usage {
+    echo "Usage:"
+    echo "  ${0##*/} region workspace_name instance_index"
+    echo "  ${0##*/} region workspace_name first_instance last_instance"
+    exit 1
+}
+
+function redeploy_instances {
+    for index in $(seq "$1" "$2")
+    do
+        # Strip control characters, then look for the text "id" surrounded by
+        # space characters, then extract only the ID from that line.
+        # The first sed line has been carefully crafted to work with BSD sed.
+        nmap_instance_id=$(terraform state show aws_instance.cyhy_nmap[$index] | \
+                               sed $'s,\x1b\\[[0-9;]*[[:alpha:]],,g' | \
+                               grep "[[:space:]]id[[:space:]]" | \
+                               sed "s/[[:space:]]*id[[:space:]]*= \"\(.*\)\"/\1/")
+
+        # Terminate the existing nmap instance
+        aws --region "$region" ec2 terminate-instances --instance-ids "$nmap_instance_id"
+        aws --region "$region" ec2 wait instance-terminated --instance-ids "$nmap_instance_id"
+
+        terraform apply -var-file="$workspace.tfvars" \
+                  -target=aws_eip_association.cyhy_nmap_eip_assocs[$index] \
+                  -target=aws_instance.cyhy_nmap[$index] \
+                  -target=aws_route53_record.cyhy_portscan_A[$index] \
+                  -target=aws_route53_record.cyhy_rev_portscan_PTR[$index] \
+                  -target=aws_volume_attachment.nmap_cyhy_runner_data_attachment[$index] \
+                  -target="module.dyn_nmap.module.cyhy_nmap_ansible_provisioner_$index"
+    done
+}
+
 if [ $# -eq 3 ]
 then
-    region=$1
-    workspace=$2
-    index=$3
+    stop=$3
+elif [ $# -eq 4 ]
+then
+    stop=$4
 else
-    echo "Usage:  deploy_new_portscan_instance.sh region workspace_name instance_index"
-    exit 1
+    usage
+fi
+
+region=$1
+workspace=$2
+start=$3
+
+if [ "$start" -gt "$stop" ]
+then
+    usage
 fi
 
 terraform workspace select "$workspace"
-
-# Strip control characters, then look for the text "id" surrounded by
-# space characters, then extract only the ID from that line.
-#
-# The first sed line has been carefully crafted to work with BSD sed.
-nmap_instance_id=$(terraform state show aws_instance.cyhy_nmap[$index] | \
-                       sed $'s,\x1b\\[[0-9;]*[[:alpha:]],,g' | \
-                       grep "[[:space:]]id[[:space:]]" | \
-                       sed "s/[[:space:]]*id[[:space:]]*= \"\(.*\)\"/\1/")
-
-# Terminate the existing nmap instance
-aws --region "$region" ec2 terminate-instances --instance-ids "$nmap_instance_id"
-aws --region "$region" ec2 wait instance-terminated --instance-ids "$nmap_instance_id"
-
-terraform apply -var-file="$workspace.tfvars" \
-          -target=aws_eip_association.cyhy_nmap_eip_assocs[$index] \
-          -target=aws_instance.cyhy_nmap[$index] \
-          -target=aws_route53_record.cyhy_portscan_A[$index] \
-          -target=aws_route53_record.cyhy_rev_portscan_PTR[$index] \
-          -target=aws_volume_attachment.nmap_cyhy_runner_data_attachment[$index] \
-          -target="module.dyn_nmap.module.cyhy_nmap_ansible_provisioner_$index"
+redeploy_instances "$start" "$stop"
