825bb11e45188ec647b2798c89680e5188164736
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-10-13 11:56:44 -0400

    Update Terraform version pinnings
    Add provider pins to `terraform/versions.tf`. Add required pins to all other
    Terraform configurations: terraform_egress_pub, terraform_nessus_only, and
    terraform_route_53.

--- terraform_nessus_only/versions.tf
+++ terraform_nessus_only/versions.tf
@@ -1,4 +1,11 @@
-
 terraform {
-  required_version = ">= 0.12"
+  # We want to hold off on 0.13 until we have tested it.
+  required_version = "~> 0.12.0"
+
+  # Pin to the latest 2.x AWS provider, since the 3.x provider is
+  # unstable and causing problems.
+  required_providers {
+    aws      = "~> 2.0"
+    template = "~> 2.1"
+  }
 }
