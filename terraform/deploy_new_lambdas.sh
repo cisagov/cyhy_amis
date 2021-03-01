d7d42261cb268fb4539634afe58c650edaf2184e
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-03-01 17:43:16 -0500

    Autoformat terraform/deploy_new_lambdas.sh with beautysh

--- terraform/deploy_new_lambdas.sh
+++ terraform/deploy_new_lambdas.sh
@@ -10,18 +10,18 @@ set -o pipefail
 workspace=prod-a
 if [ $# -ge 1 ]
 then
-    workspace=$1
+  workspace=$1
 fi
 
 terraform workspace select "$workspace"
 
 for i in $(seq 0 2)
 do
-    terraform taint "aws_lambda_function.lambdas[$i]"
+  terraform taint "aws_lambda_function.lambdas[$i]"
 done
 
 terraform apply -var-file="$workspace.tfvars" \
-          -target=aws_lambda_function.lambdas \
-          -target=aws_iam_role_policy.lambda_cloudwatch_policies \
-          -target=aws_iam_role_policy.lambda_bod_docker_policy \
-          -target=aws_cloudwatch_log_group.lambda_logs
+  -target=aws_lambda_function.lambdas \
+  -target=aws_iam_role_policy.lambda_cloudwatch_policies \
+  -target=aws_iam_role_policy.lambda_bod_docker_policy \
+  -target=aws_cloudwatch_log_group.lambda_logs
