8868b165ea82442c88cf5f5e4b055c7254de084c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-05-05 15:15:40 -0400

    Upgrade to version 3.x of the AWS provider for Terraform
    Co-authored-by: Dave Redmin <david.redmin@beta.dhs.gov>

--- terraform_egress_pub/versions.tf
+++ terraform_egress_pub/versions.tf
@@ -2,10 +2,8 @@ terraform {
   # We want to hold off on 0.13 until we have tested it.
   required_version = "~> 0.12.0"
 
-  # Pin to the latest 2.x AWS provider, since the 3.x provider is
-  # unstable and causing problems.
   required_providers {
-    aws     = "~> 2.0"
+    aws     = "~> 3.0"
     archive = "~> 2.1"
   }
 }
