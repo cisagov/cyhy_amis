e953b265762983a6a0a61de7ac528a38b9195768
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-09-02 16:03:59 -0400

    Update provider versions for terraform_egress_pub
    The result of using `terraform 0.13upgrade .` in the terraform_egress_pub
    directory and ensuring that we are pinned to only the 0.13 minor version of
    Terraform.

--- terraform_egress_pub/versions.tf
+++ terraform_egress_pub/versions.tf
@@ -3,7 +3,13 @@ terraform {
   required_version = "~> 0.13.0"
 
   required_providers {
-    aws     = "~> 3.0"
-    archive = "~> 2.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.1"
+    }
   }
 }
