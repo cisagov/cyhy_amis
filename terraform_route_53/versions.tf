ed6dea5b3b47f73c19cf40a4c0f453375ad5f072
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-09-08 11:30:57 -0400

    Update provider versions for terraform_route_53
    The result of using `terraform 0.13upgrade .` in the terraform_route_53
    directory and ensuring that we are pinned to only the 0.13 minor version of
    Terraform.

--- terraform_route_53/versions.tf
+++ terraform_route_53/versions.tf
@@ -5,6 +5,9 @@ terraform {
   # Pin to the latest 2.x AWS provider, since the 3.x provider is
   # unstable and causing problems.
   required_providers {
-    aws = "~> 2.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.0"
+    }
   }
 }
