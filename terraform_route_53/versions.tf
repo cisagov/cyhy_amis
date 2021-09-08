44110cafd1d6e0adb1fc9ccd8d96cfda8cc3e8ae
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-09-08 11:29:55 -0400

    Update versions for terraform_route_53
    Update the `versions.tf` for the configuration in terraform_route_53/ to pin
    to Terraform 0.13.

--- terraform_route_53/versions.tf
+++ terraform_route_53/versions.tf
@@ -1,6 +1,6 @@
 terraform {
-  # We want to hold off on 0.13 until we have tested it.
-  required_version = "~> 0.12.0"
+  # We want to hold off on 0.14 until we have tested it.
+  required_version = "~> 0.13.0"
 
   # Pin to the latest 2.x AWS provider, since the 3.x provider is
   # unstable and causing problems.
