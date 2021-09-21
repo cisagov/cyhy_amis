80382547f9c5692edb0fbf1b70de206d884c5651
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-09-21 11:29:04 -0400

    Update Terraform version pin for terraform
    Update the pin for the move to Terraform 1.0..

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,7 @@
 
 terraform {
-  # We want to hold off on 0.15+ until we have tested it.
-  required_version = "~> 0.14.0"
+  # We want to hold off on 1.1 or higher until we have tested it.
+  required_version = "~> 1.0"
 
   # If you use any other providers you should also pin them to the
   # major version currently being used.  This practice will help us
