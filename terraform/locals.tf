8ea32f053d8cf82a4b2a42815ef7ba8d20b64690
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-11 09:58:44 -0500

    Remove terraform/main.tf
    Move the remaining data resources into locals.tf to mirror how we
    organize things in Terraform projects now.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,9 @@
+# The list of available AWS availability zones
+data "aws_availability_zones" "all" {}
+
+# The AWS account ID being used
+data "aws_caller_identity" "current" {}
+
 locals {
   # This is a goofy but necessary way to determine if
   # terraform.workspace contains the substring "prod"
--- terraform/main.tf
+++
@@ -1,7 +0,0 @@
-# The AWS account ID being used
-data "aws_caller_identity" "current" {
-}
-
-# The list of available AWS availability zones
-data "aws_availability_zones" "all" {
-}
