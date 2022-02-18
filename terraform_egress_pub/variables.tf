2e0a306001615a1417a1942c50a4b58726d86b04
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-18 11:10:33 -0500

    Update the terraform_egress_pub/ variable definitions
    Ensure that every variable block has a type argument and sort the
    arguments by name for every variable block.

--- terraform_egress_pub/variables.tf
+++ terraform_egress_pub/variables.tf
@@ -11,32 +11,37 @@
 # ------------------------------------------------------------------------------
 
 variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
   default     = "a"
+  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
+  type        = string
 }
 
 variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  type        = string
 }
 
 variable "distribution_domain" {
-  description = "The domain name of the cloudfront distribution and certificate."
   default     = "rules.ncats.cyber.dhs.gov"
+  description = "The domain name of the cloudfront distribution and certificate."
+  type        = string
 }
 
 variable "root_object" {
-  description = "The root object to serve when no path is provided, or an error occurs"
   default     = "all.txt"
+  description = "The root object to serve when no path is provided, or an error occurs"
+  type        = string
 }
 
 variable "rules_bucket_name" {
-  description = "The name of the bucket to store egress IP addresses"
   default     = "s3-cdn.rules.ncats.cyber.dhs.gov"
+  description = "The name of the bucket to store egress IP addresses"
+  type        = string
 }
 
 variable "tags" {
-  type        = map(string)
   default     = {}
   description = "Tags to apply to all AWS resources created"
+  type        = map(string)
 }
