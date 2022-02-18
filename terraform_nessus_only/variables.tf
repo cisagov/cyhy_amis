b0883eb8cd196895d98f4ae8b868805750a60131
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-17 19:19:53 -0500

    Organize terraform_nessus_only/ variables file
    Group the variables into required and optional variables and sort them
    alphabetically by name.

--- terraform_nessus_only/variables.tf
+++ terraform_nessus_only/variables.tf
@@ -1,3 +1,29 @@
+# ------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+#
+# You must provide a value for each of these parameters.
+# ------------------------------------------------------------------------------
+
+variable "nessus_activation_codes" {
+  type        = list(string)
+  description = "A list of strings containing Nessus activation codes"
+}
+
+variable "remote_ssh_user" {
+  description = "The username to use when sshing to the EC2 instances"
+}
+
+# ------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+#
+# These parameters have reasonable defaults.
+# ------------------------------------------------------------------------------
+
+variable "aws_availability_zone" {
+  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
+  default     = "a"
+}
+
 variable "aws_region" {
   description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
@@ -5,9 +31,21 @@ variable "aws_region" {
   # default VPC, and that subnet resides in us-east-1a
 }
 
-variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
-  default     = "a"
+variable "cyhy_elastic_ip_cidr_block" {
+  description = "The CIDR block of elastic addresses available for use by CyHy scanner instances.  This is only used in Production workspaces."
+  default     = ""
+}
+
+variable "nessus_first_elastic_ip_offset" {
+  type        = number
+  description = "The offset of the address (from the start of the elastic IP CIDR block) to be assigned to the *first* Nessus instance.  For example, if the CIDR block is 192.168.1.0/24 and the offset is set to 10, the first Nessus address used will be 192.168.1.10.  This is only used in production workspaces.  Each additional Nessus instance will get the next consecutive address in the block.  NOTE: This will only work as intended when a contiguous CIDR block of EIP addresses is available.  This is only used in Production workspaces."
+  default     = 1
+}
+
+variable "nessus_instance_count" {
+  type        = number
+  description = "The number of Nessus instances to create."
+  default     = 1
 }
 
 variable "tags" {
@@ -31,30 +69,4 @@ variable "trusted_ingress_networks_ipv6" {
   type        = list(string)
   default     = ["::/0"]
   description = "Trusted IPv6 ingress networks"
-}
-
-variable "remote_ssh_user" {
-  description = "The username to use when sshing to the EC2 instances"
-}
-
-variable "nessus_instance_count" {
-  type        = number
-  description = "The number of Nessus instances to create."
-  default     = 1
-}
-
-variable "nessus_activation_codes" {
-  type        = list(string)
-  description = "A list of strings containing Nessus activation codes"
-}
-
-variable "cyhy_elastic_ip_cidr_block" {
-  description = "The CIDR block of elastic addresses available for use by CyHy scanner instances.  This is only used in Production workspaces."
-  default     = ""
-}
-
-variable "nessus_first_elastic_ip_offset" {
-  type        = number
-  description = "The offset of the address (from the start of the elastic IP CIDR block) to be assigned to the *first* Nessus instance.  For example, if the CIDR block is 192.168.1.0/24 and the offset is set to 10, the first Nessus address used will be 192.168.1.10.  This is only used in production workspaces.  Each additional Nessus instance will get the next consecutive address in the block.  NOTE: This will only work as intended when a contiguous CIDR block of EIP addresses is available.  This is only used in Production workspaces."
-  default     = 1
 }
