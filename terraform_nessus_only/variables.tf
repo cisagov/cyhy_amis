e527b223e14599661de13da014e17161ff633683
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-18 11:13:11 -0500

    Update the terraform_nessus_only/ variable definitions
    Ensure that every variable block has a type argument and sort the
    arguments by name for every variable block.

--- terraform_nessus_only/variables.tf
+++ terraform_nessus_only/variables.tf
@@ -5,12 +5,13 @@
 # ------------------------------------------------------------------------------
 
 variable "nessus_activation_codes" {
-  type        = list(string)
   description = "A list of strings containing Nessus activation codes"
+  type        = list(string)
 }
 
 variable "remote_ssh_user" {
   description = "The username to use when sshing to the EC2 instances"
+  type        = string
 }
 
 # ------------------------------------------------------------------------------
@@ -20,53 +21,56 @@ variable "remote_ssh_user" {
 # ------------------------------------------------------------------------------
 
 variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
   default     = "a"
+  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
+  type        = string
 }
 
+# Currently we need to use us-east-1a because we are using a subnet in the
+# default VPC, and that subnet resides in us-east-1a
 variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
-  # Currently we need to use us-east-1a because we are using a subnet in the
-  # default VPC, and that subnet resides in us-east-1a
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  type        = string
 }
 
 variable "cyhy_elastic_ip_cidr_block" {
-  description = "The CIDR block of elastic addresses available for use by CyHy scanner instances.  This is only used in Production workspaces."
   default     = ""
+  description = "The CIDR block of elastic addresses available for use by CyHy scanner instances.  This is only used in Production workspaces."
+  type        = string
 }
 
 variable "nessus_first_elastic_ip_offset" {
-  type        = number
-  description = "The offset of the address (from the start of the elastic IP CIDR block) to be assigned to the *first* Nessus instance.  For example, if the CIDR block is 192.168.1.0/24 and the offset is set to 10, the first Nessus address used will be 192.168.1.10.  This is only used in production workspaces.  Each additional Nessus instance will get the next consecutive address in the block.  NOTE: This will only work as intended when a contiguous CIDR block of EIP addresses is available.  This is only used in Production workspaces."
   default     = 1
+  description = "The offset of the address (from the start of the elastic IP CIDR block) to be assigned to the *first* Nessus instance.  For example, if the CIDR block is 192.168.1.0/24 and the offset is set to 10, the first Nessus address used will be 192.168.1.10.  This is only used in production workspaces.  Each additional Nessus instance will get the next consecutive address in the block.  NOTE: This will only work as intended when a contiguous CIDR block of EIP addresses is available.  This is only used in Production workspaces."
+  type        = number
 }
 
 variable "nessus_instance_count" {
-  type        = number
-  description = "The number of Nessus instances to create."
   default     = 1
+  description = "The number of Nessus instances to create."
+  type        = number
 }
 
 variable "tags" {
-  type = map(string)
   default = {
     Team        = "VM Fusion - Development"
     Application = "Manual Cyber Hygiene"
   }
   description = "Tags to apply to all AWS resources created"
+  type        = map(string)
 }
 
 # This should be overridden by a production.tfvars file,
 # most-likely stored outside of version control
 variable "trusted_ingress_networks_ipv4" {
-  type        = list(string)
   default     = ["0.0.0.0/0"]
   description = "Trusted IPv4 ingress networks"
+  type        = list(string)
 }
 
 variable "trusted_ingress_networks_ipv6" {
-  type        = list(string)
   default     = ["::/0"]
   description = "Trusted IPv6 ingress networks"
+  type        = list(string)
 }
