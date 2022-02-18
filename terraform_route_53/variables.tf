4e4ccbfdaa4edfd9b3a99186bd3e4ad29ba8e60c
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-17 19:21:07 -0500

    Organize terraform_route_53/ variables file
    Group the variables into required and optional variables and sort them
    alphabetically by name.

--- terraform_route_53/variables.tf
+++ terraform_route_53/variables.tf
@@ -1,11 +1,23 @@
-variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)."
-  default     = "us-east-1"
-}
+# ------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+#
+# You must provide a value for each of these parameters.
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+#
+# These parameters have reasonable defaults.
+# ------------------------------------------------------------------------------
 
 variable "aws_availability_zone" {
   description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
   default     = "a"
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  default     = "us-east-1"
 }
 
 variable "tags" {
