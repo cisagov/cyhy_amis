0e9e58f7e5c272f24d95810369ddec02b1a9fc89
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-18 11:14:05 -0500

    Update the terraform_route_53/ variable definitions
    Ensure that every variable block has a type argument and sort the
    arguments by name for every variable block.

--- terraform_route_53/variables.tf
+++ terraform_route_53/variables.tf
@@ -11,17 +11,19 @@
 # ------------------------------------------------------------------------------
 
 variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
   default     = "a"
+  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
+  type        = string
 }
 
 variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  type        = string
 }
 
 variable "tags" {
-  type        = map(string)
   default     = {}
   description = "Tags to apply to all AWS resources created"
+  type        = map(string)
 }
