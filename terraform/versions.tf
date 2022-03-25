5a24d2c46b022fee2e880fdfbacec1a19c3cf524
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-03-25 18:44:53 -0400

    Update the minimum version of the Terraform AWS provider
    Bump from ~> 3.62 to ~> 3.75 to support the backported changes to how
    AWS S3 bucket resources are structured in v4 of the provider.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,14 +7,12 @@ terraform {
   # major version currently being used.  This practice will help us
   # avoid unwelcome surprises.
   required_providers {
-    # Version 3.62.0 of the Terraform AWS provider adds the
-    # `stop_instance_before_detaching` argument to the `aws_volume_attachment`
-    # resource.
-    # https://github.com/hashicorp/terraform-provider-aws/pull/21144
-    # https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md#3620-october-08-2021
+    # Version 3.75.0 of the Terraform AWS provider backports the changes to how
+    # AWS S3 resources are structured from the 4.0 release.
+    # https://www.hashicorp.com/blog/terraform-aws-provider-4-0-refactors-s3-bucket-resource
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.62"
+      version = "~> 3.75"
     }
     cloudinit = {
       source  = "hashicorp/cloudinit"
