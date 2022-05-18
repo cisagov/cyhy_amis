a2b07810e569ca344266a7c53882dbb6d32cd517
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-05-18 12:55:47 -0400

    Adjust bod_docker cloud-init
    Add chowning of the `/var/log/cyhy` directory and reorder tasks so that
    chowning the `/var/cyhy` and `/var/log/cyhy` directories are the first
    shellscript tasks. Also group all cloud-config parts together.

--- terraform/bod_docker_cloud_init.tf
+++ terraform/bod_docker_cloud_init.tf
@@ -25,6 +25,28 @@ data "cloudinit_config" "bod_docker_cloud_init_tasks" {
   }
 
   part {
+    content = templatefile("${path.module}/cloud-init/chown_directory.tpl.sh", {
+      group          = "cyhy"
+      is_mount_point = false
+      owner          = "cyhy"
+      path           = "/var/cyhy"
+    })
+    content_type = "text/x-shellscript"
+    filename     = "00_cyhy_docker_chown_cyhy_directory.sh"
+  }
+
+  part {
+    content = templatefile("${path.module}/cloud-init/chown_directory.tpl.sh", {
+      group          = "cyhy"
+      is_mount_point = false
+      owner          = "cyhy"
+      path           = "/var/log/cyhy"
+    })
+    content_type = "text/x-shellscript"
+    filename     = "00_cyhy_docker_chown_cyhy_log_directory.sh"
+  }
+
+  part {
     content = templatefile("${path.module}/cloud-init/disk_setup.tpl.sh", {
       device_name   = "/dev/xvdb"
       fs_type       = "xfs"
@@ -34,7 +56,7 @@ data "cloudinit_config" "bod_docker_cloud_init_tasks" {
       num_disks     = 3
     })
     content_type = "text/x-shellscript"
-    filename     = "00_orchestrator_disk_setup.sh"
+    filename     = "01_orchestrator_disk_setup.sh"
   }
 
   part {
@@ -45,18 +67,7 @@ data "cloudinit_config" "bod_docker_cloud_init_tasks" {
       path           = "/var/cyhy/orchestrator/output"
     })
     content_type = "text/x-shellscript"
-    filename     = "01_cyhy_docker_chown_orchestrator_output_directory.sh"
-  }
-
-  part {
-    content = templatefile("${path.module}/cloud-init/chown_directory.tpl.sh", {
-      group          = "cyhy"
-      is_mount_point = false
-      owner          = "cyhy"
-      path           = "/var/cyhy"
-    })
-    content_type = "text/x-shellscript"
-    filename     = "02_cyhy_docker_chown_cyhy_directory.sh"
+    filename     = "02_cyhy_docker_chown_orchestrator_output_directory.sh"
   }
 
   part {
