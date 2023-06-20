a47453df23b44f325605ef545af33a6f1e711e8f
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2023-06-20 18:35:50 -0400

    Add additional cyhy-commander configurability
    Add the ability to configure the `jobs-per-nessus-host` and
    `jobs-per-nmap-host` values in the `commander.conf` file through the
    Terraform configuration.

--- terraform/cyhy_mongo_ec2.tf
+++ terraform/cyhy_mongo_ec2.tf
@@ -191,9 +191,12 @@ module "cyhy_mongo_ansible_provisioner" {
     "aws_region=${var.aws_region}",
     "dmarc_import_aws_region=${var.dmarc_import_aws_region}",
     "dmarc_import_es_role=${var.dmarc_import_es_role_arn}",
-    "nmap_hosts=${join(",", formatlist("portscan%d", range(1, var.nmap_instance_count + 1)))}",
+    # cyhy-commander configuration values
+    "jobs_per_nessus_host=${var.commander_config.jobs_per_nessus_host}",
+    "jobs_per_nmap_host=${var.commander_config.jobs_per_nmap_host}",
     "nessus_hosts=${join(",", formatlist("vulnscan%d", range(1, var.nessus_instance_count + 1)))}",
     "next_scan_limit=${var.commander_config.next_scan_limit}",
+    "nmap_hosts=${join(",", formatlist("portscan%d", range(1, var.nmap_instance_count + 1)))}",
   ]
   playbook = "../ansible/playbook.yml"
   dry_run  = false
