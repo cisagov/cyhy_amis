686fcdd00acf1c979048013bea0cdb5c8d82bddc
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2023-11-27 23:15:13 -0500

    Fix formatting with `terraform fmt` in terraform/

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,18 +9,18 @@ output "cyhy_lambda_mongodb_sg" {
 }
 
 output "cyhy_private_sg" {
-    description = "The security group for the private portion of the CyHy VPC."
-    value       = aws_security_group.cyhy_private_sg
+  description = "The security group for the private portion of the CyHy VPC."
+  value       = aws_security_group.cyhy_private_sg
 }
 
 output "cyhy_private_subnet" {
-    description = "The private subnet of the CyHy VPC."
-    value       = aws_subnet.cyhy_private_subnet
+  description = "The private subnet of the CyHy VPC."
+  value       = aws_subnet.cyhy_private_subnet
 }
 
 output "cyhy_public_subnet" {
-    description = "The public subnet of the CyHy VPC."
-    value       = aws_subnet.cyhy_public_subnet
+  description = "The public subnet of the CyHy VPC."
+  value       = aws_subnet.cyhy_public_subnet
 }
 
 output "cyhy_vpc" {
