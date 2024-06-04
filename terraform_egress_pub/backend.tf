f6cdec9fe2e4e234408279cc6c49344e800787b8
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2024-06-03 22:26:27 -0400

    Remove the Terraform configuration in terraform_egress_pub/
    This configuration is no longer in use as replacement functionality has
    been implemented in the cisagov/publish-egress-ip-lambda and
    cisagov/publish-egress-ip-terraform projects. Therefore, it makes sense
    to remove the configuration.

--- terraform_egress_pub/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "ncats-terraform-state-storage"
-    dynamodb_table = "terraform-state-lock"
-    region         = "us-east-1"
-    key            = "egress_pub/terraform.tfstate"
-  }
-}
