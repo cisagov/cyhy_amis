2eba4ec0d02a5ee103a77ee6f9c6b5f4d73e4331
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-17 11:35:05 -0400

    Correct an overzealous search/replace

--- terraform/databricks_user_read.tf
+++ terraform/databricks_user_read.tf
@@ -16,7 +16,7 @@ resource "aws_iam_access_key" "databricks_user_read" {
   user = aws_iam_user.databricks_user_read.name
 }
 
-# Attach the databricks S3 bucket read policy
+# Attach the MOE S3 bucket read policy
 resource "aws_iam_user_policy_attachment" "moe_bucket_read_policy_attachment_databricks_user" {
   user       = aws_iam_user.databricks_user_read.name
   policy_arn = aws_iam_policy.moe_bucket_read.arn
