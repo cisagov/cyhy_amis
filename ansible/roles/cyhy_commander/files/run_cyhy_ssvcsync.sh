aded5a4032d0a505e68005b4d9b539c20d5e5102
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-02-26 16:16:05 -0500

    Rewrite run_cyhy_ssvcsync.sh to be more foolproof and less hacky
    I also added some basic error handling.
    
    Note: I wanted to use the GitHub CLI ("gh") for this, but it is not currently possible to make unauthenticated calls with gh, so I settled for using curl and jq like a caveman.

--- ansible/roles/cyhy_commander/files/run_cyhy_ssvcsync.sh
+++ ansible/roles/cyhy_commander/files/run_cyhy_ssvcsync.sh
@@ -1,26 +1,44 @@
 #!/usr/bin/env bash
 
+# A script to run cyhy-ssvcsync to get the full set of SSVC data from the most
+# recent GitHub release of the CVEProject/cvelistV5 repository.
+#
+# The release is expected to be named in the format "CVE YYYY-MM-DD_HH00Z".
+# The zip file attached to the release is expected to be named in the format
+# "YYYY-MM-DD_all_CVEs_at_midnight.zip.zip".
+
 set -o nounset
 set -o errexit
 set -o pipefail
 
-# Get current date in UTC in the format YYYY-MM-DD
-CURRENT_DATE=$(date --utc +"%Y-%m-%d")
+# Get the recent releases for the cvelistV5 repository from the GitHub REST API
+RELEASES_JSON=$(curl --silent --user-agent "cisagov/cyhy_amis/run_cyhy_ssvcsync" https://api.github.com/repos/CVEProject/cvelistV5/releases)
 
-# Get two hours ago in UTC in the format HH
-TWO_HOURS_AGO_HOUR=$(date --utc --date="2 hours ago" +"%H")
+# Check if the API request was successful
+if [[ -z "$RELEASES_JSON" ]]; then
+  echo "Error: Failed to retrieve releases from GitHub API" >&2
+  exit 1
+fi
 
-# Build the URL for the CVEs zip file in the GitHub release based on the
-# current date and two hours ago.  Use the release from two hours ago to allow
-# plenty of time for the release to be published.
-RELEASE_URL="https://github.com/CVEProject/cvelistV5/releases/download/cve_${CURRENT_DATE}_${TWO_HOURS_AGO_HOUR}00Z/${CURRENT_DATE}_all_CVEs_at_midnight.zip.zip"
+# Find the name of the most recent release that matches the naming pattern
+# "CVE YYYY-MM-DD_HH00Z"
+RELEASE_NAME=$(echo "$RELEASES_JSON" | jq -r '.[].name' | grep -E '^CVE [0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}00Z$' | head -n 1)
 
-# Run cyhy-ssvcsync to get the full set of SSVC data from a recent (~2 hours
-# prior) GitHub release. The release is expected to be named in the format
-# "cve_YYYY-MM-DD_HH00Z". The zip file within the release is expected to be
-# named in the format "YYYY-MM-DD_all_CVEs_at_midnight.zip.zip".
-#
-# NOTE: This will not work correctly if the script is run between 0000 and 0200
-# UTC, but that is an acceptable limitation since the script is only intended to
-# be run once per day, outside of those hours.
-/usr/local/bin/cyhy-ssvcsync "$RELEASE_URL"
+# Check if a matching release was found
+if [[ -z "$RELEASE_NAME" ]]; then
+  echo "Error: No release found matching the expected naming pattern" >&2
+  exit 1
+fi
+
+# Find the URL of the asset in the most recent release that matches the naming
+# pattern "YYYY-MM-DD_all_CVEs_at_midnight.zip.zip"
+ASSET_URL=$(echo "$RELEASES_JSON" | jq -r --arg RELEASE_NAME "$RELEASE_NAME" '.[] | select(.name == $RELEASE_NAME) | .assets[] | select(.name | test("^[0-9]{4}-[0-9]{2}-[0-9]{2}_all_CVEs_at_midnight.zip.zip$")) | .browser_download_url')
+
+# Check if a matching asset was found
+if [[ -z "$ASSET_URL" ]]; then
+  echo "Error: No asset found in the release '$RELEASE_NAME' matching the expected naming pattern" >&2
+  exit 1
+fi
+
+# Run cyhy-ssvcsync with the URL of the asset from the most recent release
+/usr/local/bin/cyhy-ssvcsync "$ASSET_URL"
