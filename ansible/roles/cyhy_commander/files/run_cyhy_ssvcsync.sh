fd6c24cb93f20e354e9d21da2ca52357c7294cf9
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-03-02 11:27:28 -0500

    Break up long lines in a script to shorter, more readable lines
    Co-authored-by: Shane Frasier <jeremy.frasier@gwe.cisa.dhs.gov>

--- ansible/roles/cyhy_commander/files/run_cyhy_ssvcsync.sh
+++ ansible/roles/cyhy_commander/files/run_cyhy_ssvcsync.sh
@@ -14,7 +14,10 @@ set -o pipefail
 # Get the recent releases for the cvelistV5 repository from the GitHub REST API.
 # Since releases are returned in descending order by creation date, the most
 # recent release will be the first one in the list.
-RELEASES_JSON=$(curl --silent --user-agent "cisagov/cyhy_amis/run_cyhy_ssvcsync" https://api.github.com/repos/CVEProject/cvelistV5/releases)
+RELEASES_JSON=$(
+  curl --silent --user-agent "cisagov/cyhy_amis/run_cyhy_ssvcsync" \
+    "https://api.github.com/repos/CVEProject/cvelistV5/releases"
+)
 
 # Check if the API request was successful
 if [[ -z "$RELEASES_JSON" ]]; then
@@ -24,7 +27,10 @@ fi
 
 # Find the name of the most recent release that matches the naming pattern
 # "CVE YYYY-MM-DD_HH00Z"
-RELEASE_NAME=$(echo "$RELEASES_JSON" | jq -r '.[].name' | grep -E '^CVE [0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}00Z$' | head -n 1)
+RELEASE_NAME=$(
+  echo "$RELEASES_JSON" | jq -r '.[].name' \
+    | grep -E '^CVE [0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}00Z$' | head -n 1
+)
 
 # Check if a matching release was found
 if [[ -z "$RELEASE_NAME" ]]; then
@@ -34,11 +40,17 @@ fi
 
 # Find the URL of the asset in the most recent release that matches the naming
 # pattern "YYYY-MM-DD_all_CVEs_at_midnight.zip.zip"
-ASSET_URL=$(echo "$RELEASES_JSON" | jq -r --arg RELEASE_NAME "$RELEASE_NAME" '.[] | select(.name == $RELEASE_NAME) | .assets[] | select(.name | test("^[0-9]{4}-[0-9]{2}-[0-9]{2}_all_CVEs_at_midnight.zip.zip$")) | .browser_download_url')
+ASSET_URL=$(
+  echo "$RELEASES_JSON" | jq -r --arg RELEASE_NAME "$RELEASE_NAME" \
+    '.[] | select(.name == $RELEASE_NAME) | .assets[] | select(.name |
+    test("^[0-9]{4}-[0-9]{2}-[0-9]{2}_all_CVEs_at_midnight.zip.zip$")) |
+    .browser_download_url'
+)
 
 # Check if a matching asset was found
 if [[ -z "$ASSET_URL" ]]; then
-  echo "Error: No asset found in the release '$RELEASE_NAME' matching the expected naming pattern" >&2
+  echo "Error: No asset found in the release '$RELEASE_NAME'" \
+    "matching the expected naming pattern" >&2
   exit 1
 fi
 
